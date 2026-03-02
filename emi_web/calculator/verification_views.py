c4036c1cc256ed68d04f57a099fdd2fb04141df0
Piyush nanda <137023372+piyush0130@users.noreply.github.com>
2026-03-02 15:43:48 +0530

    Update verification_views.py

--- emi_web/calculator/verification_views.py
+++ emi_web/calculator/verification_views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
 
+def robots_txt_view(request):
+    robots_content = "User-agent: *\nAllow: /\n"
+    return HttpResponse(robots_content, content_type="text/plain")
+
 def google_verification_view(request):
     return HttpResponse("google-site-verification: googleb66a2a1986aafb83.html")
+
